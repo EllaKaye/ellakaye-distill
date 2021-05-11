988da7b24d942b32cb566066dfa0c526fa32555b
Ella Kaye <ella.kaye@gmail.com>
2021-05-11 12:26:14 +0100

    Brighter syntax highlighting and a couple of fixes to welcome post

--- R/syntax_highlighting.R
+++ R/syntax_highlighting.R
@@ -1,7 +1,22 @@
 # Manual
 
-theme <- readLines(here::here("arrow.theme"))
+theme
 
+
+#arrow_theme <- system.file("rmarkdown/templates/distill_article/resources/arrow.theme", package = "distill")
+
+# brighter version
+theme <- gsub("#AD0000", "#D40067", theme) # red -> pink
+theme <- gsub("#8f5902", "#C15500", theme) # brown -> orange (note lower case!)
+theme <- gsub("#007BA5", "#6600D4", theme) # blue -> purple
+theme <- gsub("#20794D", "#008747", theme) # green -> green
+theme <- gsub("#4758AB", "#0077CE", theme) # purple -> blue
+
+writeLines(theme, "ek_syntax_highlighting.theme")
+
+
+# darker version
+theme <- readLines(here::here("arrow.theme"))
 theme <- gsub("#AD0000", "#C3005F", theme) # red -> pink
 theme <- gsub("#8F5902", "#C15400", theme) # brown -> orange
 theme <- gsub("#007BA5", "#5E00C2", theme) # blue -> purple
--- _site/R/syntax_highlighting.R
+++ _site/R/syntax_highlighting.R
@@ -2,6 +2,21 @@
 
 theme <- readLines(here::here("arrow.theme"))
 
+
+#arrow_theme <- system.file("rmarkdown/templates/distill_article/resources/arrow.theme", package = "distill")
+
+# brighter version
+theme <- gsub("#AD0000", "#D40067", theme) # red -> pink
+theme <- gsub("#8F5902", "#C15500", theme) # brown -> orange
+theme <- gsub("#007BA5", "#6600D4", theme) # blue -> purple
+theme <- gsub("#20794D", "#008747", theme) # green -> green
+theme <- gsub("#4758AB", "#0077CE", theme) # purple -> blue
+
+writeLines(theme, "ek_syntax_highlighting.theme")
+
+
+# darker version
+theme <- readLines(here::here("arrow.theme"))
 theme <- gsub("#AD0000", "#C3005F", theme) # red -> pink
 theme <- gsub("#8F5902", "#C15400", theme) # brown -> orange
 theme <- gsub("#007BA5", "#5E00C2", theme) # blue -> purple
