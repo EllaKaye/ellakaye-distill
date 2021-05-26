7f47c5d7b3d30678655182482448ff7156642320
Ella Kaye <ella.kaye@gmail.com>
2021-05-26 15:09:17 +0100

    set up highlighting post (#2)
    * set up highlighting post
    
    * Tweak blue in highlighting theme and outline syntax highlighting post
    
    * update syntax_highlighting with comment about changing background colour NOT working
    
    * _site update after tweak blue in syntax highlighting
    
    * write a few more sections
    
    * Tweaks to green, purple and brown in syntax highlighting scheme
    
    * update and flesh out post outline
    
    * first draft of colour theory section
    
    * tweak to main pink and highlighting theme
    
    * tweak pink in logo and add more images to highlighting post
    
    * add combination image
    
    * finish colour theory sections
    
    * Annoted HSL wheel
    
    * add R code for HCL approach
    
    * finish accessibility section
    
    * finish colourblindness section
    
    * Delete superflous css in syntax post
    
    * add palette in R section
    
    * modifying the theme section
    
    * TL;DR
    
    * start spin-off part 2 post
    
    * change Fira Code for Fira Mono (to make it easier for others to copy code chunks)
    
    * split content between parts 1 and 2
    
    * pink links on hover and focus
    
    * applying theme section
    
    * completed draft of Part 1, ready for proof-reading
    
    * simplify distilltools section and update a couple of pictures
    
    * a few more part 1 tweaks, and copy theme for distillery
    
    * tiny tweak to theme, fig.alt in Part 1 and progress on Part 2
    
    * more improvements to Part 2
    
    * finishing off Part 2
    
    * fix colour codes and images
    
    * Add bib and colorspace reference
    
    * rename post dir for both highlighting posts
    
    * rebuild site after renaming syntax highlighting posts
    
    * Bring part 1 forward a day to keep posts in order
    
    * add links between parts 1 and 2
    
    * Both parts ready to go!

--- _site/R/syntax_highlighting.R
+++ _site/R/syntax_highlighting.R
@@ -1,22 +1,22 @@
 # Manual
 
-# theme <- readLines("arrow.theme")
+theme <- readLines("arrow.theme")
 # theme taken from rmarkdown/templates/distill_article/resources/arrow.theme in the "distill" package and also accessible via
 
 # arrow_theme_path <- system.file("rmarkdown/templates/distill_article/resources/arrow.theme", package = "distill")
 
 # Could readLines directly from arrow_theme_path, but better to copy to my root directory first, in case distill team tweak colours in the package version, which would stop the gsub/str_replace_all calls from working!
 
-# file.copy(arrow_theme_path, "templates/arrow.theme")
+# file.copy(arrow_theme_path, "arrow.theme")
 
 # Now this is done once, keep above lines commented and don't rerun!
 
 # brighter version
-theme <- gsub("#AD0000", "#D40067", theme) # red -> pink
-theme <- gsub("#8f5902", "#C15500", theme) # brown -> orange (note lower case!)
-theme <- gsub("#007BA5", "#6600D4", theme) # blue -> purple
-theme <- gsub("#20794D", "#008747", theme) # green -> green
-theme <- gsub("#4758AB", "#0077CE", theme) # purple -> blue
+theme <- gsub("#AD0000", "#B6005B", theme) # red -> pink
+theme <- gsub("#8f5902", "#B65B00", theme) # brown -> brown (note lower case!)
+theme <- gsub("#007BA5", "#5B00B6", theme) # blue -> purple
+theme <- gsub("#20794D", "#008643", theme) # green -> green
+theme <- gsub("#4758AB", "#005BB6", theme) # purple -> blue
 
 writeLines(theme, "ek_syntax_highlighting.theme")
 
@@ -28,11 +28,11 @@ writeLines(theme, "ek_syntax_highlighting.theme")
 # library(magrittr)
 #
 # theme <- readLines("arrow.theme") %>%
-#   str_replace_all("#AD0000", "#D40067") %>% # red -> pink
-#   str_replace_all("#8f5902", "#C15500") %>% # brown -> orange (note lower case!)
-#   str_replace_all("#007BA5", "#6600D4") %>% # blue -> purple
-#   str_replace_all("#20794D", "#008747") %>% # green -> green
-#   str_replace_all("#4758AB", "#0077CE")     # purple -> blue
+#   str_replace_all("#AD0000", "#BD005B") %>% # red -> pink
+#   str_replace_all("#8f5902", "#B65D00") %>% # brown -> brown (note lower case!)
+#   str_replace_all("#007BA5", "#5A00B5") %>% # blue -> purple
+#   str_replace_all("#20794D", "#008643") %>% # green -> green
+#   str_replace_all("#4758AB", "#005BB6")     # purple -> blue
 #
 # writeLines(theme, "ek_syntax_highlighting.theme")
 
@@ -51,3 +51,22 @@ writeLines(theme, "ek_syntax_highlighting.theme")
 
 # Actually, have just heard from Alison Hill that the R Markdown team are working on pandoc package that will include wrapper functions to make your own highlighter, so might hold off on wrapping this in a function/distilltools as their version will be the canonical approach.
 
+# this DOESN'T seem to work to change the background colour
+#theme <- readLines("arrow.theme")
+#theme[3] <- "    \"background-color\": \"#FDDB97\","
+#theme[3]
+#theme[4]
+#writeLines(theme, "yellow_background.theme")
+
+# library(stringr)
+# library(magrittr)
+#
+# test_theme <- readLines("arrow.theme") %>%
+#   str_replace_all("#AD0000", "#BD005B") %>% # red -> pink
+#   str_replace_all("#8f5902", "#B65D00") %>% # brown -> brown (note lower case!)
+#   str_replace_all("#007BA5", "#90f") %>% # blue -> purple
+#   str_replace_all("#20794D", "#f60") %>% # green -> green
+#   str_replace_all("#4758AB", "#005BB6")     # purple -> blue
+#
+# writeLines(test_theme, "test.theme")
+# doesn't work with short codes
